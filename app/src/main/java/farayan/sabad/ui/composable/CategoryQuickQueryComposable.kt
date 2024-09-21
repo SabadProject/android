bd07953b71b777d96db2c89cb1691ff3c274ff5c
Hassan (Homayoun) Behzadian <6245340+behzadian@users.noreply.github.com>
2024-09-21 11:26:01 +0200

    WIP: (#9)
    Co-authored-by: Homayoun <eqpc@hbn.dev>

--- app/src/main/java/farayan/sabad/ui/composable/CategoryQuickQueryComposable.kt
+++ app/src/main/java/farayan/sabad/ui/composable/CategoryQuickQueryComposable.kt
@@ -3,6 +3,7 @@ package farayan.sabad.ui.composable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import farayan.sabad.R
 import farayan.sabad.utility.appFont
 
@@ -30,6 +32,8 @@ fun CategoryQuickQueryComposable(
             textStyle = TextStyle(fontFamily = appFont),
             label = { Text(text = stringResource(id = R.string.category_quick_query_label), fontFamily = appFont) },
             modifier = Modifier.weight(1.0f),
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
             trailingIcon = {
                 if (addable) {
                     Icon(imageVector = Icons.Default.Add, contentDescription = "add", modifier = Modifier.clickable { onAddClicked() })
